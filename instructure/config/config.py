01e66c3a07ddf08370e623dfcc3fb7dd47f8ed81
truejomi <pixgel13040@gmail.com>
2023-11-17 18:18:46 -0500

    json

--- instructure/config/config.py
+++ instructure/config/config.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials,firestore_async, storage
 
-cred_firebase = credentials.Certificate("./domain/static/credentials_firebase.json")
+cred_firebase = credentials.Certificate("./domain/static/credentials_firebase1.json")
 fire = firebase_admin.initialize_app(cred_firebase)
 db =  firestore_async.client()
 
